750304c4b9acd693e90e7dd4e7cedb65e76d9d41
zhangweiwei <345765558@163.com>
2019-03-27 10:55:28 +0800

    11
    11111

---
+++ 小象微信端.air/小象微信端.py
@@ -0,0 +1,50 @@
+# -*- encoding=utf8 -*-
+__author__ = "zhangweiwei"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from airtest_selenium.proxy import WebChrome
+
+import random
+def create_phone():
+    # 第二位数字
+    second = [3, 4, 5, 7, 8][random.randint(0, 4)]
+    # 第三位数字
+    third = {
+        3: random.randint(0, 9),
+        4: [5, 7, 9][random.randint(0, 2)],
+        5: [i for i in range(10) if i != 4][random.randint(0, 8)],
+        7: [i for i in range(10) if i not in [4, 9]][random.randint(0, 7)],
+        8: random.randint(0, 9),
+    }[second]
+    # 最后八位数字
+    suffix = random.randint(9999999,100000000)
+    # 拼接手机号
+    return "1{}{}{}".format(second, third, suffix)
+# 生成手机号
+phone = create_phone()
+
+
+
+企业货主 = WebChrome()
+企业货主.implicitly_wait(20)
+企业货主.get("http://192.168.4.182/#/")
+
+'''
+个人货主 = WebChrome()
+个人货主.implicitly_wait(20)
+个人货主.get("http://192.168.4.182/#/")
+'''
+
+经纪人 = WebChrome()
+经纪人.implicitly_wait(20)
+经纪人.get("http://192.168.4.182/#/")
+
+司机 = WebChrome()
+司机.implicitly_wait(20)
+司机.get("http://192.168.4.182/#/")
+
