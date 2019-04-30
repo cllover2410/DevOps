52c52cd6b33cc61534d6cd7a0db15429e85ae88a
cllover2410 <40019460+cllover2410@users.noreply.github.com>
2019-04-30 15:19:38 +0800

    add
    add

--- django1.11/mysite/mysite/urls.py
+++ django1.11/mysite/mysite/urls.py
@@ -8,6 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
+        添加导入：从其他视图导入主页
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
--- django1.11/mysite/mysite/wsgi.py
+++ django1.11/mysite/mysite/wsgi.py
@@ -1,7 +1,9 @@
 """
 WSGI config for mysite project.
+用于mysite项目的wsgi配置。
 
 It exposes the WSGI callable as a module-level variable named ``application``.
+它将wsgi可调用公开为一个名为``application``的模块级变量。
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
